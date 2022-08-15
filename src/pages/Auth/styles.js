e4146f2248a5aa3251f6b5726247dcc16a21041b
Lucas Alves Rodrigues <contato.lucasalv@gmail.com>
2022-08-15 05:44:09 -0300

    style: favicon

--- src/pages/Auth/styles.js
+++ src/pages/Auth/styles.js
@@ -90,7 +90,7 @@ export const Form = styled.form`
 		color: #2d3051;
 		filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
 		:hover {
-			background-color: #432a78;
+			background-color: #f1f2f3;
 		}
 	}
 `
