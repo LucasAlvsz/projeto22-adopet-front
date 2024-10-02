e1ebdfe338214c375b2d15b4ee29c605b03fecd3
Lucas Alves <contato.lucasalv@gmail.com>
2024-10-02 03:52:37 +0000

    refactor: Update styles in Discover page
    - Adjusted the height of the Discover component to fit the viewport
    - Added margin-top to create spacing between the component and the header
    - Increased padding in larger screens for better spacing
    - Adjusted the height of the Container component to fill the available space
    - Removed margin-bottom in larger screens to center the content
    
    Closes #123

--- src/pages/Discover/styles.js
+++ src/pages/Discover/styles.js
@@ -1,24 +1,29 @@
 import styled from "styled-components"
 
 export const Discover = styled.div`
-	width: 100%;
-	height: 100%;
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	justify-content: center;
-	padding: 20px;
-	background-color: #f2f4f3;
+  width: 100%;
+  height: calc(100vh - 135px);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 20px;
+  margin-top: 45px;
+  background-color: #f2f4f3;
+
+  @media (min-width: 768px) {
+    padding: 40px;
+  }
 `
 
 export const Container = styled.div`
-	width: 100%;
-	height: 75%;
+  width: 100%;
+  height: 100%;
+  padding-bottom: 20px;
 
-	@media (min-width: 768px) {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		margin-bottom: 65px;
-	}
+  @media (min-width: 768px) {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
 `
